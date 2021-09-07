cd1c6e5282469574bc13c86ad58b14e27681dd39
Simon <simonrwangwrn@gmail.com>
2021-09-07 15:07:57 -0400

    More Psuedocode and JUnit Tests

--- src/Board.java
+++ src/Board.java
@@ -2,11 +2,11 @@
 public class Board {
 
     Board(int size) {
-
+        //TODO: Construct the Board and make sure it's a valid size
     }
 
     // TODO: setState <state>
-    private void setState(String state) {
+    String setState(String state) {
         //if state is not null
 
             //then if state has the all the proper values for the specified puzzle size
@@ -26,32 +26,83 @@ public class Board {
     // TODO: printState
     public String printState() {
         String state = "";
+        //for each character in the string
+
+            //if the counter < max counter
+
+                //print character + “ “
+
+            //else
+
+                //print “/n” + character + “ “
+
         return state;
     }
 
     // TODO: move <direction>
-    private void move(String direction) {
+    String move(String direction) {
+        //If direction is not null
+
+            //then if direction is a valid direction
+
+                    //then swap blank with character in direction
+
+                //else
+
+                    //error: invalid direction
+        //else
+
+                //error: direction given is null
 
     }
 
     // TODO: randomizeState <n>
-    private void randomizeState(int n) {
+    String randomizeState(int n) {
+        //Counter = n
+
+        //lastDirection = “”
+
+        //directionlist of north south east west
+
+        //removedlist of “”
+
+        //For I of n
+
+            //If lastDirection is not “”
+
+                //Remove last direction from directionlist, add to removedlist
+
+                //Randomized routine(directionlist, removedlist)
+
+            //else
+
+                //randomized routine(directionlist, removedlist)
+
+                //Reset directionlist and removed list
+
 
     }
 
-    // TODO: solveAStar <heuristic>
-    private void solveAStar(int heuristic) {
+    //randomizedroutine ( dlist, rlist)
+    //If blank is touching wall
+    //Remove directions that touch wall, add to removedlist
+    //Pick a random number from 1-directionlist size
+    //move(direction)
+    //add directions from removedlist to directionlist, clear removedlist
 
+    // TODO: solveAStar <heuristic>
+    void solveAStar(int heuristic) {
+        //TODO: PSUEDOCODE
     }
 
     // TODO: solveBeam <k>
-    private void solveBeam(int k) {
-
+    void solveBeam(int k) {
+        //TODO: PSUEDOCODE
     }
 
     // TODO: maxNodes <n>
     private void maxNodes(int n) {
-
+        //TODO: PSUEDOCODE
     }
 
 }
--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
@@ -10,22 +12,29 @@ class BoardTest {
 
     public void setStateTest() {
         //set board as a custom arrangement
-
+        assertEquals("574638201", board.setState("574638201"));
         //set board as arrangement in order
-
+        assertEquals("012345678", board.setState("012345678"));
         //set board as above with one tiny change
+        assertEquals("312045678", board.setState("312045678"));
     }
 
     public void printStateTest() {
         //set board as a custom arrangement, then test print
-
+        board.setState("574638201");
+        assertEquals("5 7 4 n/6 3 8 n/2 0 1 ", board.printState());
         //set board as arrangement in order, then test print
-
+        board.setState("012345678");
+        assertEquals("0 1 2 n/3 4 5 n/6 7 8 ", board.printState());
         //set board as above with one tiny change, then test print
+        board.setState("312045678");
+        assertEquals("3 1 2 n/0 4 5 n/6 7 8 ", board.printState());
     }
 
     public void randomizeStateTest() {
+        Random random = new Random();
         //randomize board, check to make sure board isnt the same as regular
+        assertNotEquals("012345678", board.randomizeState(random.nextInt(19)+1));
     }
 
     public void solveAStarTest() {
