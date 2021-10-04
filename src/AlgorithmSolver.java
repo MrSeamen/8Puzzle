5950ab6a2077f4c8e8c1bbe22eb1e196e3717fe1
Simon <simonrwangwrn@gmail.com>
2021-10-04 12:10:34 -0400

    restructuring of classes to separate algorithm from board class to reduce complexity

---
+++ src/AlgorithmSolver.java
@@ -0,0 +1,9 @@
+public class AlgorithmSolver {
+    //max nodes
+    //current heuristic type
+    //node subclass - contains board, last move, path cost, heuristic cost, f(n) equation, and parent board
+
+    //AStar
+
+    //Beam Search
+}
